8db22202567f6ad1de31425f25a22f85cd17d7e8
cloudstrider <fancifulcorona@gmail.com>
2017-02-03 12:17:10 -0800

    Deleted a whole crapton of copied and commented out text and revised one comment. Progress has been made...

--- Ground_Control_2017/display.py
+++ Ground_Control_2017/display.py
@@ -15,7 +15,7 @@ Text = gw.Text
 Entry  = gw.Entry
 Image = gw.Image
 
-#Set up static values
+#Set up Global
 inputFile = "input1.txt" # name of file to read input from
 outputFile = "savedData.txt" # name of file where data is recorded
 aTempMax = 1000 # the temp/press when the bars are full
@@ -60,11 +60,14 @@ class DataWindow(threading.Thread):
         self.altData.line.setSize(32)
         self.speedData = DataField(self.window, Point(780,30), "(m/s)")
         self.acclData = DataField(self.window, Point(1050,30), "(m/s^2)")
+        
         self.aTempData = DataWithBar(self.window, Point(950,200), "*C",Point(750,190),Point(1150,210), aTempMax, "red")
         self.bTempData = DataWithBar(self.window, Point(950,250), "*C",Point(750,240),Point(1150,260), bTempMax, "red")
         self.cTempData = DataWithBar(self.window, Point(950,300), "*C",Point(750,290),Point(1150,310), bTempMax, "red")
+        
         self.yLabel = DataField(self.window, Point(40,225), "")
         self.xLabel = DataField(self.window, Point(540,545), "")
+        
         self.press = DataWithBar(self.window,Point(975,100),"(atm)",Point(775,90),Point(1175,110),pressMax, "blue")
         self.dataFields = (self.altData, self.speedData, self.acclData, self.aTempData, self.bTempData, self.cTempData, self.yLabel, self.xLabel, self.press)
         
@@ -264,140 +267,3 @@ def convert(points, origin):
         p.setFill("blue")
         newPoints.append(p)
     return newPoints
-
-
-
-###################################################
-
-#handled by data application file
-#window = GraphWin("Data",1200,600) # window object
-#setUp(window)
-
-"""
-# initialize tracked data and stuff
-speed = 0 # m/s
-altitude = 0 # m
-currentAltMax = 1000.0 # graph starts with max at 1 km
-currentTMax = 5.0 # graph starts with max at 5 seconds
-origin = Point(75,525) # pixel at graph origin
-yMax = 315 # pixel length of y axis
-xMax = 465 # pixel length of x axis
-points = [Point(0,0),] # the data recorded and scaled
-displayPoints = [origin,] # the data displayed
-displayLines = [] # the connecting lines
-
-
-# initialize data fields
-altData = DataField(window, Point(350,75), "(m)")
-altData.line.setSize(32)
-speedData = DataField(window, Point(780,30), "(m/s)")
-acclData = DataField(window, Point(1050,30), "(m/s^2)")
-aTempData = DataWithBar(window, Point(950,200), "*C",Point(750,190),Point(1150,210), aTempMax, "red")
-bTempData = DataWithBar(window, Point(950,250), "*C",Point(750,240),Point(1150,260), bTempMax, "red")
-cTempData = DataWithBar(window, Point(950,300), "*C",Point(750,290),Point(1150,310), bTempMax, "red")
-yLabel = DataField(window, Point(40,225), "")
-xLabel = DataField(window, Point(540,545), "")
-press = DataWithBar(window,Point(975,100),"(atm)",Point(775,90),Point(1175,110),pressMax, "blue")
-dataFields = (altData, speedData, acclData, aTempData, bTempData, cTempData, yLabel, xLabel, press)
-
-
-#window.getMouse() # click to start
-
-
-pause = Text(Point(1125,575),"P") # make the pause button
-pause.setSize(20)
-pause.setTextColor("blue")
-pause.draw(window)
-
-stop = Rectangle(Point(1160,557),Point(1190,587)) # make stop button
-stop.setFill("red")
-stop.draw(window)
-
-
-# initialize output
-output = open(outputFile, "a")
-output.write("////////// " + time.asctime(time.localtime(time.time())) + " //////////\n")
-output.write("Time (min, s) Accl (m/s^2)  y-speed (m/s) altitude (m)  aTemp (*C)    bTemp (*C)    cTemp (*C)    pressure (atm)\n")
-
-running = 1
-while running == 1:
-    timeStart = time.time()
-    
-    # collect data for manipulation (acclY, aTemp, bTemp, cTemp)
-    inputData = open(inputFile, "r")
-    currentData = []
-    for i in range(0,5):
-        data = inputData.readline()
-        if data[0:len(data)-1]:
-            currentData.append(round(float(data[0:len(data)-1]),1))
-        else:
-            currentData.append("NO")
-    #print(currentData)
-    inputData.close()
-
-    # approximate alt
-    if currentData[0] != "NO":
-        speed += float(currentData[0]) * sampleTime
-    altitude += speed * sampleTime
-
-    # set data fields
-    dataFields[0].set(altitude)
-    dataFields[1].set(speed)
-    dataFields[2].set(currentData[0])
-    dataFields[3].set(currentData[1])
-    dataFields[4].set(currentData[2])
-    dataFields[5].set(currentData[3])
-    dataFields[6].set(currentAltMax)
-    dataFields[7].set(currentTMax)
-    dataFields[8].set(currentData[4])
-
-    # graph:
-    points.append(Point(points[-1].getX() + xMax * sampleTime / currentTMax, altitude * yMax / currentAltMax))
-    while altitude > currentAltMax:
-        oldMax = currentAltMax
-        currentAltMax  = currentAltMax * 1.5
-        for i in range(0,len(points)):
-            points[i] = Point(points[i].getX(), points[i].getY() * oldMax / currentAltMax)
-    while points[-1].getX() > xMax:
-        oldMax = currentTMax
-        currentTMax += 10
-        for i in range(0,len(points)):
-            points[i] = Point(points[i].getX() * oldMax / currentTMax, points[i].getY())
-    for p in displayPoints:
-        p.undraw()
-    for l in displayLines:
-        l.undraw()
-    displayPoints = convert(points, origin)
-    oldP = origin
-    for p in displayPoints:
-        p.draw(window)
-        if len(displayPoints) < xMax:
-            l = Line(p,oldP)
-            l.setFill("Blue")
-            l.draw(window)
-            displayLines.append(l)
-            oldP = p
-
-    # record output
-    recordTime = str(time.localtime(time.time())[4]) + ", " + str(round(time.localtime(time.time())[5] + math.modf(time.time())[0],2))
-    record(output, 14, (recordTime, currentData[0], round(speed, 2), round(altitude, 2), currentData[1], currentData[2], currentData[3], currentData[4]))
-    
-    # check if paused or stopped
-    click = window.checkMouse()
-    if click and int(click.getX()) in range(1150,1195) and int(click.getY()) in range(550,595):
-        running = 0
-        #window.close()
-    elif click and int(click.getX()) in range(1100,1149) and int(click.getY()) in range(550,595):
-        pause.undraw()
-        window.getMouse()
-        pause.draw(window)
-
-    if time.time() - timeStart > sampleTime:
-        print("TIME! " + str(round(time.time() - timeStart, 3)))
-    while time.time() - timeStart < sampleTime:
-        time.sleep(0.01)
-    #print("Done!")
-
-output.close()
-window.close()
-"""
